0004f3d590366bd8857d9e8f083a6440b48a78e8
Hassan Khan <hakhan0301@gmail.com>
2018-06-24 17:10:25 -0500

    added model

--- data/data-reader.js
+++ data/data-reader.js
@@ -8,6 +8,7 @@ module.exports = {
     currentIndex = 0;
   },
 
+  // error handling isn't implemented, so that means if count gets high enough, currentIndex can give indexoutofbounds
   nextSet: function(count) {
     const set = {
       features: [],
@@ -15,15 +16,23 @@ module.exports = {
     };
 
     for(let i = 0; i < count; i++) {
-      set.features.push(data.features[currentIndex]);
-      set.labels.push(data.labels[currentIndex]);
+      set.features.push(tf.tensor1d(data.features[currentIndex]));
+      set.labels.push(tf.tensor1d(data.labels[currentIndex]));
       currentIndex++;
     }
 
-    set.features = tf.tensor2d(set.features);
-    set.labels = tf.tensor2d(set.labels);
+    // set.features = tf.tensor2d(set.features);
+    // set.labels = tf.tensor2d(set.labels);
 
     return set;
+  },
+
+  fullSet: function() {
+    return this.nextSet (this.size());
+  },
+
+  size: function() {
+    return data.labels.length;
   },
 
   getData: function() {
--- index.js
+++ index.js
@@ -3,8 +3,54 @@ const dataReader = require('./data/data-reader');
 
 dataReader.initializeData('./parsed-data/test-data.json');
 
-let data = dataReader.nextSet(3);
-data.labels.print();
+const model = createModel(0.07);
 
-data = dataReader.nextSet(4);
-data.labels.print();
+// test();
+
+async function train (setSize, validationPercentage) {
+
+}
+
+async function test () {
+  dataReader.initializeData('./parsed-data/test-data.json');
+  const data = tf.tidy(() => dataReader.fullSet());
+  console.log(tf.memory().numTensors);
+  console.log(tf.memory().numBytes);
+
+}
+
+function createModel(learningRate) {
+  const output = tf.sequential();
+
+  output.add(tf.layers.conv2d({
+    inputShape: [784, 1, 1],
+    kernelSize: 4,
+    strides: 1,
+    filters: 8,
+    activation: 'sigmoid'
+  }));
+  output.add(tf.layers.maxPooling2d({
+    poolSize: [2, 2]
+  }));
+  output.add(tf.layers.conv2d({
+    kernelSize: 4,
+    strides: 1,
+    filters: 8,
+    activation: 'sigmoid'
+  }));
+  output.add(tf.layers.maxPooling2d({
+    poolSize: [2, 2]
+  }));
+  output.add(tf.layers.flatten());
+  output.add(tf.layers.dense({
+    units: 10,
+    activation: 'sigmoid'
+  }));
+
+  output.compile({
+    optimizer: tf.train.sgd(learningRate),
+    loss: tf.losses.meanSquaredError,
+  });
+
+  return output;
+}
