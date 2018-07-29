7df6e71e512c251cb06f63e2c05959db1e9864b1
Hassan Khan <hakhan0301@gmail.com>
2018-07-29 15:18:53 -0500

    added validation loss to training callback

--- index.js
+++ index.js
@@ -1,95 +1,115 @@
-const tf = require('@tensorflow/tfjs');
-const dataReader = require('./data/data-reader');
+const tf = require("@tensorflow/tfjs");
+const dataReader = require("./data/data-reader");
 
 const model = createModel(0.17);
 
-test(10000);
-train(0, 1000).then(() => test(10000));
+// test(10000);
+train(0, 10).then(() => test(10000));
 
+function test(count) {
+	dataReader.initializeData("./parsed-data/test-data.json");
+	const data = tf.tidy(() => dataReader.nextSet(count));
+	dataReader.clearData();
 
-function test (count) {
-  dataReader.initializeData('./parsed-data/test-data.json');
-  const data = tf.tidy(() => dataReader.nextSet(count));
-  dataReader.clearData();
+	const options = {
+		batchSize: 10,
+		epochs: 1,
+		callbacks: {
+			onBatchEnd: (index, logs) => {
+				console.log(logs);
+				printTensors();
+			}
+		}
+	};
 
-  console.log('started evaluating data');
-  model.evaluate(data.features, data.labels).print();
-  dataReader.disposeData(data);
-  console.log('\n');
+	console.log("started evaluating data");
+	model.evaluate(data.features, data.labels).print();
+	dataReader.disposeData(data);
+	console.log("\n");
 }
 
-async function train (fileNumber, count) {
-  let filePath = './parsed-data/train-data-' + fileNumber + '.json';
+async function train(fileNumber, count) {
+	let filePath = "./parsed-data/train-data-" + fileNumber + ".json";
 
-  dataReader.initializeData(filePath);
-  const data = tf.tidy(() => dataReader.nextSet(count));
-  dataReader.clearData();
+	dataReader.initializeData(filePath);
+	const data = tf.tidy(() => dataReader.nextSet(count));
+	dataReader.clearData();
 
-  console.log(tf.memory().numTensors);
-  console.log("started training");
+	console.log(tf.memory().numTensors);
+	console.log("started training");
 
-  const options = {
-    batchSize: 10,
-    epochs: 1, 
-    validationSplit: .2,
-    callbacks: {
-      onBatchEnd: (index, logs) => {
-        console.log(logs);
-        console.log(tf.memory().numTensors);
-      }
-    }
-  }
+	const options = {
+		batchSize: 10,
+		epochs: 1,
+		validationSplit: 0.2,
+		metrics: ["loss", "val_loss"],
+		callbacks: {
+			onBatchEnd: (index, logs) => {
+				console.log(logs);
+				printTensors();
+			}
+		}
+	};
 
-  const h = await model.fit(data.features, data.labels, options);
-  console.log(h.history.loss[0]);
-  dataReader.disposeData(data);
-  console.log('\n');
+	const h = await model.fit(data.features, data.labels, options);
+	console.log(h.history);
+	dataReader.disposeData(data);
+	console.log("\n");
 }
-
 
 function createModel(learningRate) {
-  const model = tf.sequential();
+	const model = tf.sequential();
 
-  model.add(tf.layers.conv1d({
-    inputShape: [28, 28],
-    kernelSize: 5,
-    filters: 8,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'VarianceScaling'
-  }));
-  model.add(tf.layers.maxPooling1d({
-    poolSize: 4,
-    strides: 4
-  }));
-  model.add(tf.layers.conv1d({
-    kernelSize: 5,
-    filters: 16,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'VarianceScaling'
-  }));
-  model.add(tf.layers.maxPooling1d({
-    poolSize: 2,
-    strides: 4
-  }));
-  model.add(tf.layers.flatten());
-  model.add(tf.layers.dense({
-    units: 10,
-    kernelInitializer: 'VarianceScaling',
-    activation: 'softmax'
-  }));
+	model.add(
+		tf.layers.conv1d({
+			inputShape: [28, 28],
+			kernelSize: 5,
+			filters: 8,
+			strides: 1,
+			activation: "relu",
+			kernelInitializer: "VarianceScaling"
+		})
+	);
+	model.add(
+		tf.layers.maxPooling1d({
+			poolSize: 4,
+			strides: 4
+		})
+	);
+	model.add(
+		tf.layers.conv1d({
+			kernelSize: 5,
+			filters: 16,
+			strides: 1,
+			activation: "relu",
+			kernelInitializer: "VarianceScaling"
+		})
+	);
+	model.add(
+		tf.layers.maxPooling1d({
+			poolSize: 2,
+			strides: 4
+		})
+	);
+	model.add(tf.layers.flatten());
+	model.add(
+		tf.layers.dense({
+			units: 10,
+			kernelInitializer: "VarianceScaling",
+			activation: "softmax"
+		})
+	);
 
-  const optimizer = tf.train.sgd(learningRate);  
+	const optimizer = tf.train.sgd(learningRate);
 
-  model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-  });
+	model.compile({
+		optimizer: optimizer,
+		loss: "categoricalCrossentropy"
+	});
 
-  return model;
+	return model;
 }
 
 function printTensors() {
-  console.log('tensor count: ' + tf.memory().numTensors);
+	console.log("tensor count: " + tf.memory().numTensors);
 }
